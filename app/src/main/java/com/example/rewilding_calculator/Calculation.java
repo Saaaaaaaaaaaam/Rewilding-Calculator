9897bab6ecc10a3934fd81118c2a9d5162c9e83b
Saaaaaaaaaaaam <40437441@live.napier.ac.uk>
2021-05-20 15:32:57 +0100

    early part of Calculation

--- app/src/main/java/com/example/rewilding_calculator/Calculation.java
+++ app/src/main/java/com/example/rewilding_calculator/Calculation.java
@@ -1,4 +1,30 @@
 package com.example.rewilding_calculator;
 
 public class Calculation {
+    final static double airCarbon=23;
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    private double cost=0;
+
+    public double Calculate(int stage, double input){
+        if(stage==0){
+        return AirMiles(input);
+        }else if(stage==1){
+
+        }
+
+
+
+        return -1;
+    }
+
+    private double AirMiles(double miles){
+        return miles*airCarbon;
+    }
 }
--- app/src/main/java/com/example/rewilding_calculator/MainActivity.java
+++ app/src/main/java/com/example/rewilding_calculator/MainActivity.java
@@ -10,33 +10,45 @@ import android.text.method.NumberKeyListener;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-private  static  final  String Tag = "MainActivity";
+    private static final String Tag = "MainActivity";//Only needed for debugging
+    public Calculation calc = new Calculation(); //Object declared globally
+
+//    ListView output = findViewById(R.id.listview);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//Declare the starting layout
+        EditText input = findViewById(R.id.editTextNumber);//Define input box
+//        Button b1 = findViewById(R.id.button);//
+        TextView working = findViewById(R.id.textView);
+        TextView display = findViewById(R.id.textView2); //Define the output box
 
-        EditText input = findViewById(R.id.editTextNumber);
-        Button b1 = findViewById(R.id.button);
-        TextView display = findViewById(R.id.textView2);
-        input.setOnEditorActionListener(editorActionListener);
+        input.setOnEditorActionListener(editorActionListener); //listener for when enter is pressed
 
     }
-    private  TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
+
+    /**
+     * When Enter is pressed, the following code will run
+     */
+    private TextView.OnEditorActionListener editorActionListener = new TextView.OnEditorActionListener() {
         @Override
         public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            TextView working = findViewById(R.id.textView);
 //            Log.d();
-            Log.d(Tag,"Enter Pressed");
+            Log.d(Tag, "Enter Pressed");//debug log
+            calc.setCost(calc.getCost() + Double.parseDouble(String.valueOf(v.getText())));//increase cost in calculation by the value entered by the user
+//            calc.setCost(calc.getCost() + 2);
+            Log.d(Tag, "Count"+String.valueOf(calc.getCost()));//debug log
+            working.setText(String.valueOf(v.getText())+working.getText() + "\n +"+ calc.getCost());
             return false;
         }
     };
-
 }
-
-
-
